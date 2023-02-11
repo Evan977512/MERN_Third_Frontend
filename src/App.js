8fab4ed77603070a2f90aac77b5e71fddfcced6f
EvanKim <youngchan.kim@edu.sait.ca>
2023-02-11 00:46:56 -0700

    Users props created

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 
-import Users from './user/pages/Users';
-import NewPlace from './places/pages/NewPlace'
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
-{/**
+{
+  /**
   Redirect component leads to the default path
-  Switch component will 
-*/}
+  Switch component will match the exact component that you want to redirect
+*/
+}
 const App = () => {
   return (
     <div>
       <Router>
         <Switch>
-
-          <Route path = "/" component={Users} exact/>
-
-          <Route path = "/places/new" component={NewPlace} exact/>
-
+          <Route path="/" component={Users} exact />
+          <Route path="/places/new" component={NewPlace} exact />
           <Redirect to="/" />
-
         </Switch>
       </Router>
     </div>
   );
-}
+};
 
 export default App;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-    return (
-        <h2>Users Component</h2>
-    )
-}
+  const USERS = [{ id: "u1", name: "Evan Kim", image: null, places: 3 }];
+
+  return <UsersList items={USERS} />;
+};
 
 export default Users;
