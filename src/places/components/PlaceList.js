766d455aa4cd11aa9797a31cd71aaf16d2251347
EvanKim <youngchan.kim@edu.sait.ca>
2023-02-14 13:02:31 -0700

    place component created

---
+++ src/places/components/PlaceList.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import Card from "../../shared/components/UIEelement/Card";
+import PlaceItem from "./PlaceItem";
+import "./PlaceList.css";
+
+const PlaceList = (props) => {
+  if (props.items.length === 0) {
+    return (
+      <div className="place-list center">
+        <Card>
+          <h2>No Places Found. Create New One!</h2>
+          <button>Share Place</button>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <ul className="place-list">
+      {props.item.map((place) => (
+        <PlaceItem key={place.id} id={place.id} image={place.imageUrl} title={place.title} description={place.description} address={place.address} creatorId={place.creator} coordinates={place.location} />
+      ))}
+    </ul>
+  );
+};
+
+export default PlaceList;
---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+/**
+ *
+ * @returns user places
+ */
+const UserPlaces = () => {
+  return;
+};
+
+export default UserPlaces;
