311cc2774076e68d51ba8d0302eeb8113d6ce22c
Evan Kim <evan977512@gmail.com>
2023-02-17 12:23:18 -0700

    useCallback hook created in NewPlace and Input

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useCallback } from "react";
 
-import Input from '../../shared/components/FormElements/Input';
-import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
-import './NewPlace.css';
+import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import "./NewPlace.css";
 
 const NewPlace = () => {
-    return (
-        <form className="place-form">
-            {/**
-             * validation component
-             */}
-            <Input element="input" type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a valid title" />
-        </form>
-    );
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
+  return (
+    <form className="place-form">
+      {/**
+       * validation component
+       */}
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title"
+        onInput={titleInputHandler}
+      />
+      <Input
+        it="description"
+        element="textarea"
+        type="Description"
+        label="Title"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at leat 5 characters)"
+        onInput={descriptionInputHandler}
+      />
+    </form>
+  );
 };
 
 export default NewPlace;
