4a2bbbc5e0b8117b72c6f6946cdf9c67b1e23ffc
EvanKim <youngchan.kim@edu.sait.ca>
2023-02-16 17:00:37 -0700

    input validator created

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
+import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
 import './NewPlace.css';
 
 const NewPlace = () => {
@@ -9,7 +10,7 @@ const NewPlace = () => {
             {/**
              * validation component
              */}
-            <Input element="input" type="text" label="Title" validator={[]} validators={[]} errorText="Please enter a valid title" />
+            <Input element="input" type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a valid title" />
         </form>
     );
 };
