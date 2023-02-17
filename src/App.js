46a5735d9e7d16bc3cb0e39e55d0298b478b27e1
Evan Kim <evan977512@gmail.com>
2023-02-17 13:37:40 -0700

    UpdatePlace component created

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import UserPlaces from "./places/pages/UserPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UpdatePlace from "./places/pages/UpdatePlace";
 
-{
-  /**
+/**
   Redirect component leads to the default path
   Switch component will match the exact component that you want to redirect
 */
-}
 const App = () => {
   return (
     <div>
@@ -24,6 +23,7 @@ const App = () => {
               <UserPlaces />
             </Route>
             <Route path="/places/new" component={NewPlace} exact />
+            <Route path="/places/:placeId" component={UpdatePlace}></Route>
             <Redirect to="/" />
           </Switch>
         </main>
