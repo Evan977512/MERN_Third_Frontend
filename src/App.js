8f178aebae95ce2eab153cc138ca9eea6c868a45
EvanKim <youngchan.kim@edu.sait.ca>
2023-02-20 15:27:44 -0700

    auth.js created

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
-import Users from "./user/pages/Users";
-import NewPlace from "./places/pages/NewPlace";
-import UserPlaces from "./places/pages/UserPlaces";
-import MainNavigation from "./shared/components/Navigation/MainNavigation";
-import UpdatePlace from "./places/pages/UpdatePlace";
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+import Auth from './user/pages/Auth';
+import UpdatePlace from './places/pages/UpdatePlace';
 
 /**
   Redirect component leads to the default path
   Switch component will match the exact component that you want to redirect
 */
 const App = () => {
-  return (
-    <div>
-      <Router>
-        <MainNavigation />
-        <main>
-          <Switch>
-            <Route path="/" component={Users} exact />
-            <Route path="/:userId/Places" exact>
-              <UserPlaces />
-            </Route>
-            <Route path="/places/new" component={NewPlace} exact />
-            <Route path="/places/:placeId" component={UpdatePlace}></Route>
-            <Redirect to="/" />
-          </Switch>
-        </main>
-      </Router>
-    </div>
-  );
+    return (
+        <div>
+            <Router>
+                <MainNavigation />
+                <main>
+                    <Switch>
+                        <Route path="/" component={Users} exact />
+                        <Route path="/:userId/Places" exact>
+                            <UserPlaces />
+                        </Route>
+                        <Route path="/places/new" component={NewPlace} exact />
+                        <Route path="/places/:placeId" component={UpdatePlace}></Route>
+                        <Route path="/auth" component={Auth}></Route>
+                        <Redirect to="/" />
+                    </Switch>
+                </main>
+            </Router>
+        </div>
+    );
 };
 
 export default App;
