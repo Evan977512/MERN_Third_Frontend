8b19e334357513b993bc535a952b54a0ce9da0fe
EvanKim <youngchan.kim@edu.sait.ca>
2023-02-20 13:57:04 -0700

    Edit function created

--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -2,7 +2,6 @@
  * useReducer hook also allows you to manage satae in a component
  * the difference between the useState and useReducer, you can manage more complex state
  */
-
 import React, { useReducer, useEffect } from 'react';
 
 import { validate } from '../../util/validators';
@@ -29,9 +28,9 @@ const inputReducer = (state, action) => {
 
 const Input = (props) => {
     const [inputState, dispatch] = useReducer(inputReducer, {
-        value: props.value || '',
+        value: props.initialValue || '',
         isTouched: false,
-        isValid: props.valid || false,
+        isValid: props.initialValid || false,
     });
 
     const { id, onInput } = props;
