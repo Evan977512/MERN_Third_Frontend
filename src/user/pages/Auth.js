05935ba4cd59ff373f916d06c169d0f7120cdd97
EvanKim <evan977512@gmail.com>
2023-03-20 15:43:09 -0600

    user custom HTTP hook updated but error...

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -8,13 +8,13 @@ import LoadingSpinner from "../../shared/components/UIElement/LoadingSpinner";
 import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../shared/util/validators";
 import { useForm } from "../../shared/hooks/form-hook";
 import AuthContext from "../../shared/context/auth-context";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 import "./Auth.css";
 
 const Auth = (props) => {
   const auth = useContext(AuthContext);
   const [isLoginMode, setIsLoginMode] = useState(true);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
   const [formState, inputHandler, setFormData] = useForm(
     {
@@ -64,68 +64,45 @@ const Auth = (props) => {
 
     // check if it is login mode
     if (isLoginMode) {
+      // fetch() is a built-in function in JavaScript that allows us to make HTTP requests
+      // '' => needs a string that points at backend
       try {
-        setIsLoading(true);
-        // fetch() is a built-in function in JavaScript that allows us to make HTTP requests
-        // '' => needs a string that points at backend
-        const response = await fetch(`http://localhost:5001/api/users/login`, {
-          method: "POST",
-          // HTTP 헤더는 클라이언트와 서버가 요청 또는 응답으로 부가적인 정보를 전송할 수 있도록 해줍니다
-          headers: {
-            "Content-Type": "application/json",
-          },
-          // stringify() 메서드는 배열이나 객체와 같은 JS데이터를 JSON 문자열로 변환합니다.
-          body: JSON.stringify({
+        await sendRequest(
+          "http://localhost:5001/api/users/login",
+          "POST",
+          JSON.stringify({
             email: formState.inputs.email.value,
             password: formState.inputs.password.value,
           }),
-        });
-        // returns a promise -> we need 'await' for the promise to resolve
-        const responseData = await response.json();
-        if (!response.ok) {
-          // 200번대 코드가 아닌 경우 에러를 던집니다.
-          throw new Error(responseData.message);
-        }
-        setIsLoading(false);
+          {
+            "Content-Type": "application/json",
+          }
+        );
         auth.login();
-      } catch (error) {
-        setIsLoading(false);
-        setError(error.message || "Something went wrong, please try again.");
-      }
+      } catch (error) {}
     } else {
       try {
-        setIsLoading(true);
-        const response = await fetch(`http://localhost:5001/api/users/signup`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
+        await sendRequest(
+          `http://localhost:5001/api/users/signup`,
+          "POST",
+          JSON.stringify({
             name: formState.inputs.name.value,
             email: formState.inputs.email.value,
             password: formState.inputs.password.value,
           }),
-        });
-        const responseData = await response.json();
-        if (!response.ok) {
-          throw new Error(responseData.message);
-        }
-        setIsLoading(false);
-        auth.login();
-      } catch (error) {
-        setIsLoading(false);
-        setError(error.message || "Something went wrong, please try again.");
-      }
-    }
-  };
+          {
+            "Content-Type": "application/json",
+          }
+        );
 
-  const errorHandler = () => {
-    setError(null);
+        auth.login();
+      } catch (error) {}
+    }
   };
 
   return (
     <React.Fragment>
-      <ErrorModal error={error} onClear={errorHandler} />
+      <ErrorModal error={error} onClear={clearError} />
       <Card className="authentication">
         {/**isLoading is true, then show the loading spinner*/}
         {isLoading && <LoadingSpinner asOverlay />}
