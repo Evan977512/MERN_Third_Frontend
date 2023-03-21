e64aaa960e760374004e418daa4fa17d30cad526
Evan Kim <evan977512@gmail.com>
2023-03-21 17:14:25 -0600

    http-hook updated

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -3,43 +3,28 @@ import React, { useEffect, useState } from "react";
 import ErrorModal from "../../shared/components/UIElement/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElement/LoadingSpinner";
 import UsersList from "../components/UsersList";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 // https://w0.peakpx.com/wallpaper/285/684/HD-wallpaper-fat-yoshi.jpg
 const img = "../../img/fat_yoshi.jpeg";
 
 const Users = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const [loadedUsers, setLoadedUsers] = useState(false);
 
   useEffect(() => {
-    const sendReqeust = async () => {
-      setIsLoading(true);
-
+    const fetchUsers = async () => {
       try {
-        const response = await fetch(`http://localhost:5001/api/users`);
-        const responseData = await response.json();
-
-        if (!response.ok) {
-          throw new Error(responseData.message);
-        }
+        const responseData = await sendRequest(`http://localhost:5001/api/users`);
 
         setLoadedUsers(responseData.users);
-        setIsLoading(false);
-      } catch (error) {
-        setError(error.message);
-      }
-      setIsLoading(false);
+      } catch (error) {}
     };
-    sendReqeust();
-  }, []);
-
-  const errorHandler = () => {
-    setError(null);
-  };
+    fetchUsers();
+  }, [sendRequest]);
 
   return (
     <React.Fragment>
-      <ErrorModal error={error} onClear={errorHandler} />
+      <ErrorModal error={error} onClear={clearError} />
       {isLoading && (
         <div className="center">
           <LoadingSpinner />
